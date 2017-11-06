37739ca487d0c3eb0f7db1a6f8e3bcba3128302a
장혁 <hyuk.jang@smsoft.co.kr>
2017-11-06 09:51:23 +0900

    @ index 수정

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
 const baseUtil = require('./baseUtil');
 const newUtil = require('./newUtil');
+const domUtil = require('./domUtil');
+const salternUtil = require('./salternUtil');
+require('./setJavascript');
 
 module.exports = {
   baseUtil,
-  newUtil
+  newUtil,
+  domUtil,
+  salternUtil
 }
